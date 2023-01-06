aa9f6098a9520678770812aec43255fe7110049c
unknown <kwizzyandy@gmail.com>
2023-01-06 09:15:27 +0200

    front-end

---
+++ src/router/AppRouter.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route,Redirect } from 'react-router-dom';
+import Header from '../components/Header';
+import AddBook from '../components/AddBooks';
+import BooksList from '../components/BookList';
+import useLocalStorage from '../hooks/useLocalStorage';
+import EditBook from '../components/EditBook';
+const AppRouter = () => {
+  const [books, setBooks] = useLocalStorage('books', []);
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <div className="main-content">
+          <Switch>
+          <Route
+          
+              render={(props) => (
+                <BooksList {...props} books={books} setBooks={setBooks} />
+              )}
+              path="/"
+              exact={true}
+            />
+            <Route
+  render={(props) => (
+    <AddBook {...props} books={books} setBooks={setBooks} />
+  )}
+  path="/add"
+/>
+<Route
+  render={(props) => (
+    <EditBook {...props} books={books} setBooks={setBooks} />
+  )}
+  path="/edit/:id"
+/>
+<Route component={() => <Redirect to="/" />} />
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
